d5d6fd9a129b9a5bfdf76d71aab9a78a9d2f4a52
Masab-M <masabm263@gmail.com>
2022-05-28 20:28:25 +0500

    Links

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -5,6 +5,7 @@ export default function Links() {
   return (
     <div>
         <Link to={'userOne/login'}>login</Link>
+        <br />
         <Link to={'userOne/dashBoard'}>DashBoard</Link>
     </div>
   )
